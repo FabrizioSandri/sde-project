487c13d56aa5c7a044de0dd26f49e8521f7152b2
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-11-27 17:13:41 +0100

    db interface

---
+++ adapter/database_interface/app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MySQL database configuration
+const dbConfig = {
+  host: 'database',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.DB_PORT
+};
+
+const pool = mysql.createPool(dbConfig);
+
+// Set up routes
+app.get('/', (req, res) => {
+  res.send('MySQL db interface');
+});
+
+// Authentication endpoints: login and registration
+app.get('/findUser', (req, res) => {
+  
+  const email = req.query.email;
+  const password = req.query.password;
+
+  if (!email){
+    return res.status(400).json({
+      status: "error",
+      msg: "The email is required for login"
+    });
+  }
+
+  if (!password){
+    return res.status(400).json({
+      status: "error",
+      msg: "The password is required for login"
+    });
+  }
+  
+  let query = `SELECT * FROM users WHERE email="${email}" AND password="${password}";`;
+  pool.query(query, (err, data) => {
+    if (err){
+      return res.status(400).json({
+        status: "error",
+        msg: err
+      });
+    }
+
+    // user not found
+    if (data.length == 0){
+      return res.status(404).json({
+        status: "error",
+        msg: "User not found"
+      });
+    }
+
+    // else user found
+    return res.status(200).json({
+      status: "success",
+      msg: "User found",
+      user_info: {
+        name: data[0].name,
+        surname: data[0].surname,
+        email: data[0].email,
+      }
+    });
+
+  });
+  
+});
+
+// Start the server
+const PORT = process.env.DB_ADAPTER_SERVER_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ adapter/database_interface/mysql-dump/users.sql
@@ -0,0 +1,57 @@
+-- MariaDB dump 10.19-11.1.3-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: access
+-- ------------------------------------------------------
+-- Server version	11.1.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` char(30) NOT NULL,
+  `surname` char(30) NOT NULL,
+  `email` char(50) NOT NULL,
+  `password` char(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(1,'Fabrizio','Sandri','fs@mail.com','pass1'),
+(2,'Andrea','Lorenzetti','al@mail.com','pass2'),
+(3,'Eros','Ribaga','er@mail.com','pass3');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-27 15:57:17
