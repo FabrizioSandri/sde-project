89c0c63a391f7769b4c25c198a5a7bcc9de0f62d
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-11-29 20:56:30 +0100

    authentication token

--- process_centric/authentication/routes/routes.js
+++ process_centric/authentication/routes/routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('../controllers/passport').passport;
+const controller = require("../controllers/controller")
 const normal_controller = require("../controllers/normal_controller")
 const oauth_controller = require("../controllers/oauth_controller")
-const isAuthenticated = require("../middleware/middleware").isAuthenticated
+const onlyAuthenticated = require("../middleware/middleware").onlyAuthenticated
 
 router.get('/', (req, res) => {
   res.send('Welcome to the authentication interface');
 });
+
+// Utilities endpoints and authentication status
+router.get('/isAuthenticated', controller.isAuthenticated);
+router.get('/getUserInfo', onlyAuthenticated, controller.getUserInfo);
 
 // Normal authentication
 router.post('/login', normal_controller.login);
@@ -16,8 +21,6 @@ router.post('/registration', normal_controller.registration);
 // Oauth authentication
 router.get('/google/authenticate', oauth_controller.authenticate);
 router.get('/google/callback', oauth_controller.callback, oauth_controller.afterLogin);
-router.get('/google/getUserData', isAuthenticated, oauth_controller.getUserData);
-router.get('/google/isAuthenticated', oauth_controller.isAuthenticated);
 router.get('/google/logout', oauth_controller.logout);
 
 
