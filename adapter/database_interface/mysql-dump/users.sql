1a7e2809b94a4b6250721ec9b1d7bbc8bb3bb3ea
Andrea Lorenzetti <lorenzetti889@gmail.com>
2023-11-30 09:22:05 +0100

    add database endpoints for football interesets

--- adapter/database_interface/mysql-dump/users.sql
+++ adapter/database_interface/mysql-dump/users.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19-11.1.3-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.2.0, for Linux (x86_64)
 --
--- Host: localhost    Database: access
+-- Host: localhost    Database: db
 -- ------------------------------------------------------
--- Server version	11.1.3-MariaDB
+-- Server version       8.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,18 +16,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `followedTeams`
+--
+
+DROP TABLE IF EXISTS `followedTeams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `followedTeams` (
+  `userId` mediumint NOT NULL,
+  `leagueId` int NOT NULL,
+  `teamId` int NOT NULL,
+  PRIMARY KEY (`userId`,`teamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `followedTeams`
+--
+
+LOCK TABLES `followedTeams` WRITE;
+/*!40000 ALTER TABLE `followedTeams` DISABLE KEYS */;
+INSERT INTO `followedTeams` VALUES (1,135,487),(2,135,488),(3,135,489);
+/*!40000 ALTER TABLE `followedTeams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `name` char(30) NOT NULL,
-  `surname` char(30) NOT NULL,
-  `email` char(50) NOT NULL,
-  `password` char(64) DEFAULT NULL,
+  `id` mediumint NOT NULL AUTO_INCREMENT,
+  `name` char(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` char(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` char(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -38,10 +63,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-(1,'Fabrizio','Sandri','fs@mail.com','pass1'),
-(2,'Andrea','Lorenzetti','al@mail.com','pass2'),
-(3,'Eros','Ribaga','er@mail.com','pass3');
+INSERT INTO `users` VALUES (1,'Fabrizio','Sandri','fs@mail.com','pass1'),(2,'Andrea','Lorenzetti','al@mail.com','pass2'),(3,'Eros','Ribaga','er@mail.com','pass3');   
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-27 15:57:17
+-- Dump completed on 2023-11-29 13:17:20
