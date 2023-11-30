e5d6dfa4824a92f1f1b62a02fca752ca2ffb2e11
Andrea Lorenzetti <lorenzetti889@gmail.com>
2023-11-30 09:23:07 +0100

    weather adaper

---
+++ adapter/weather_api/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/getWeather',(req,res)=>{
+    if (!req.query.lat || ! req.query.lon){
+      return res.status(400).json({
+          status:'error',
+          msg:'one or more coordinates are missing'
+      });
+    }
+
+    const options = {
+      method: 'GET',
+      url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily',
+      params: {
+        lat: req.query.lat,
+        lon: req.query.lon
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.X_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+      }
+    };
+    
+        axios.request(options)
+      .then((result)=>{
+        res.send(result.date);
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+    
+    })
+
+const PORT = process.env.WEATHER_ADAPTER_SERVER_PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+    
