54d5a8d38b006e3fe8149b21f62f5a22ab5046a2
Eros <erosribaga@hotmail.it>
2023-12-02 15:30:54 +0100

    html adapter

--- adapter/html_adapter/app.py
+++ adapter/html_adapter/app.py
@@ -1,18 +1,22 @@
-from urllib import urlopen
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import os
-
 from urllib.request import urlopen
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():    
-    fd = urlopen("http://www.python.org/")
+    return "Welcome to the HTML adapter"
+
+@app.route('/getHtml', methods=['POST']) 
+def article_html():
+    article_url = request.get_json()['url']
+
+    fd = urlopen(article_url)
     content = fd.read()
     fd.close()
 
-    return jsonify(content.decode('utf8'))
+    return jsonify({"html": content.decode('utf8')})
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ["HTML_ADAPTER_SERVER_PORT"])
